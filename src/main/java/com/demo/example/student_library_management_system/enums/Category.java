7e1c45a3ed4355ec99c19b2f1687435624ef7127
Sethu Meenakshi <sethumeenakshi91@gmail.com>
2025-05-01 15:45:10 +0530

    Model and relationships are created

---
+++ src/main/java/com/demo/example/student_library_management_system/enums/Category.java
@@ -0,0 +1,13 @@
+package com.demo.example.student_library_management_system.enums;
+
+public enum Category {
+    //enum- it is predefined datatype which contains constants for the specific fields
+    DRAMA,
+    MYTHOLOGY,
+    HORROR,
+    TECHNICAL,
+    NON_TECHNICAL,
+    THRILLER,
+    FICTION
+
+}
